dfc2b880c5778120522354ea6ce22dcec4f3aa24
yuberalberto <yuberalberto@gmail.com>
2026-05-17 22:09:48 -0400

    fix: correct load_config call to enable .env loading

--- rentals_assistant/__main__.py
+++ rentals_assistant/__main__.py
@@ -4,7 +4,7 @@ from rentals_assistant.bot import start_bot
 from rentals_assistant.config import configure_logging, load_config
 from rentals_assistant.scheduler import start
 
-configure_logging(load_config(_env_file=None).log_level)
+configure_logging(load_config().log_level)
 
 if "--bot" in sys.argv:
     start_bot()
